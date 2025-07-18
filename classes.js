408f51b8388ce7628edc0be074e1aa7b1cfa1621
Alek-Grujic <alek.grujic001@outlook.com>
2025-07-18 08:43:20 +0200

    Add simple Car class

--- classes.js
+++ classes.js
@@ -1,1 +1,17 @@
-console.log('test');
+
+class Car {
+    constructor(brand, model, year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year
+    }
+    info() {
+        console.log(`This is a ${this.brand} ${this.model} from ${this.year}`)
+    }
+}
+
+const toyota = new Car('Toyota', 'Corolla', 2010.);
+const bmw = new Car('BMW', 'X5', 2022.);
+
+console.log(toyota);
+console.log(bmw);

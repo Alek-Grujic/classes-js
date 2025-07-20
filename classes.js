a207831711b792cd228dfb834338ae495ba94826
Alek-Grujic <alek.grujic001@outlook.com>
2025-07-20 11:31:30 +0200

    Create Circle class that extends Shape using super for inheritance

--- classes.js
+++ classes.js
@@ -37,50 +37,93 @@
 
 // ------------------------------------------------------
 
-const _radius = new WeakMap();
-const _move = new WeakMap();
+// const _radius = new WeakMap();
+// const _move = new WeakMap();
 
-class Circle {
-    constructor(radius) {
-        _radius.set(this, radius);
-        _move.set(this, () => {
-            console.log('move', this);
-        });
+// class Circle {
+//     constructor(radius) {
+//         _radius.set(this, radius);
+//         _move.set(this, () => {
+//             console.log('move', this);
+//         });
+//     }
+//     draw() {
+//         _move.get(this)();
+
+//         console.log('draw');
+//     }
+// }
+
+// const c = new Circle(10);
+
+// console.log(c);
+
+// c.draw();
+
+// ------------------------------------------------------
+
+// const _secretCode = new WeakMap();
+
+// class Device {
+//     constructor(name, secretCode) {
+//         this.name = name;
+//         _secretCode.set(this, secretCode)
+//     }
+//     revealCode() {
+//         return _secretCode.get(this);
+//     }
+// }
+
+// const d = new Device('TV', 9988);
+
+// console.log(d);
+
+// console.log(d.secretCode);
+
+// console.log(d.revealCode());
+
+// ------------------------------------------------------
+
+// class Animal {
+//     constructor(name) {
+//         this.name = name
+//     }
+//     speak() {
+//         console.log("Animal makes a sound");
+//     }
+// }
+
+// class Dog extends Animal {
+
+//     bark() {
+//         console.log("Dog barks");
+//     }
+// }
+
+// const d = new Dog('Bobby');
+// d.speak(); // Nasleđeno iz Animal 
+// d.bark();  // Definisano u Dog 
+// console.log(d);
+
+// ------------------------------------------------------
+
+class Shape {
+    constructor(color) {
+        this.color = color;
+    }
+    move() {
+        console.log('move');
+    }
+}
+
+class Circle extends Shape {
+    constructor(color, radius) {
+        super(color);
+        this.radius = radius;
     }
     draw() {
-        _move.get(this)();
-
         console.log('draw');
     }
 }
 
-const c = new Circle(10);
-
-console.log(c);
-
-c.draw();
-
-// ------------------------------------------------------
-
-const _secretCode = new WeakMap();
-
-class Device {
-    constructor(name, secretCode) {
-        this.name = name;
-        _secretCode.set(this, secretCode)
-    }
-    revealCode() {
-        return _secretCode.get(this);
-    }
-}
-
-const d = new Device('TV', 9988);
-
-console.log(d);
-
-console.log(d.secretCode);
-
-console.log(d.revealCode());
-
-
-
+const c = new Circle('red', 1);
